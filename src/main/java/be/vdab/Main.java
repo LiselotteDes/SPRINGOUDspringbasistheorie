5baa8e62c727134cca3040d271a5eac9c3ca1a12
liselotte <liselotte@DESKTOP-LLSE5T0.ADM.LOCAL>
2018-10-18 17:09:34 +0200

    3.3 Een bean vragen

--- src/main/java/be/vdab/Main.java
+++ src/main/java/be/vdab/Main.java
@@ -12,6 +12,19 @@ public class Main {
 		
 		// De constructor parameter is de naam van het Spring configuratiebestand.
 		try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("container.xml")) {
+			
+			// *** Een bean vragen adhv zijn id ***
+			/*
+			System.out.println(context.getBean("classA", ClassA.class).getBoodschap());
+			System.out.println(context.getBean("classB", InterfaceB.class).getBoodschap());
+			*/
+			
+			// *** Een bean vragen adhv zijn class of interface ***
+			
+			// Als slechts 1 bean bestaat v/e class, kan je die bean vragen met de method getBean, waarbij je enkel die class opgeeft die bij de bean hoort
+			System.out.println(context.getBean(ClassA.class).getBoodschap());
+			// Als slechts 1 bean bestaat, waarvan de class een interface implementeert, kan je die bean vragen met de method getBean, waarbij je die interface opgeeft.
+			System.out.println(context.getBean(InterfaceB.class).getBoodschap());
 		}
 	}
 }
