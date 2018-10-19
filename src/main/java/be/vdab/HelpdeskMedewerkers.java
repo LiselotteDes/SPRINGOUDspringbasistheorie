c3b308d677d67e351e66354ea770e44721b5a64e
liselotte <liselotte@DESKTOP-LLSE5T0.ADM.LOCAL>
2018-10-19 11:30:20 +0200

    4.5 Een Map injecteren

--- src/main/java/be/vdab/HelpdeskMedewerkers.java
+++ src/main/java/be/vdab/HelpdeskMedewerkers.java
@@ -1,24 +1,33 @@
 package be.vdab;
 
+import java.util.Map;
+
 //import java.util.Arrays;
 //import java.util.List;
-import java.util.Set;
+//import java.util.Set;
 
-// *** Een array, List of Set injecteren ***
+// *** Een array, List of Set / Map injecteren ***
 class HelpdeskMedewerkers {
 
 	//private String[] namen;		// array
 	//private List<String> namen;	// List
-	private Set<String> namen;		//Set
+	//private Set<String> namen;	//Set
+	private Map<String, Integer> medewerkers;
+	// key = naam medewerker, value = intern telefoonnummer
 	
-	public void setNamen(/*String[]*/ /*List*/Set<String> namen) {
-		this.namen = namen;
+	//public void setNamen(/*String[]*/ /*List*/Set<String> namen) {
+		//this.namen = namen;
+	//}
+	
+	HelpdeskMedewerkers(Map<String, Integer> medewerkers) {
+		this.medewerkers = medewerkers;
 	}
 	
 	@Override
 	public String toString() {
 		//return Arrays.toString(namen);
-		return namen.toString();
+		//return namen.toString();
+		return medewerkers.toString();
 	}
 	
 }
